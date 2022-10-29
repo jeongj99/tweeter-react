7498ee99b1fcc0d066b5b99601e4e55de48ce8f1
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-10-28 22:53:37 -0300

    Import components and add them to the div

--- react-version/src/App.js
+++ react-version/src/App.js
@@ -1,9 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navigation from './components/Navigation';
+import Profile from './components/Profile';
+import TweetForm from './components/TweetForm';
+import TweetList from './components/TweetList';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Navigation></Navigation>
+      <Profile></Profile>
+      <TweetForm></TweetForm>
+      <TweetList></TweetList>
+    </div>
   );
 }
 
